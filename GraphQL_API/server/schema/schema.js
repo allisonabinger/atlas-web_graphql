38da4222beecc5e6e8e1e5a5d88a8930cccfa3a9
Allison <allisonabinger@gmail.com>
2024-07-02 15:55:31 -0500

    a - 07/02 03:55 PM - updated schema, haven't gotten a return object

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,15 +1,33 @@
 // Task 0: creates an object which contains the schema property
 
 const graphql = require('graphql')
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql
 
-const TaskType = new graphql.GraphQLObjectType({
+const TaskType = new GraphQLObjectType({
     name: 'Task',
     fields: {
-        id: { type: graphql.GraphQLString },
-        title: { type: graphql.GraphQLString },
-        weight: { type: graphql.GraphQLInt },
-        description: { type: graphql.GraphQLString },
+        id: { type: GraphQLString },
+        title: { type: GraphQLString },
+        weight: { type: GraphQLInt },
+        description: { type: GraphQLString },
     }
 });
 
-module.exports = TaskType;
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        task: {
+            type: TaskType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parent, args) {
+                // mock object
+                return { id: args.id, title: 'sample title', weight: 5, description: 'mock object'}
+            }
+        }
+    }
+});
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
