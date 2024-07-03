9bc92e1dc51a9e19d20c04e2237613ac29faed58
Allison <allisonabinger@gmail.com>
2024-07-03 13:33:08 -0500

    A - 07/03 01:33 PM - added mongo connection to app.js

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const schema = require('./schema/schema')
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose')
 
 const app = express();
+
+const mongoURI = 'mongodb+srv://allisonabinger:root@cluster01.oqci0pb.mongodb.net/'
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to mongoDB')
+});
 
 app.use('/graphql',graphqlHTTP({
     schema,
